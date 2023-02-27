e04759a387855c5752a6c1fc407b2b3f9a7034de
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-02-27 13:05:46 +0000

    Add files via upload

---
+++ all_scraper.py
@@ -0,0 +1,191 @@
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime, timedelta
+from latest_user_agents import get_random_user_agent
+
+post_item_list =[]
+item_list = []
+
+def add_up(data, url, link, header, sentence, my_date):
+    author_twitter = str(data.loc[data['Article URL'] == url, 'Author Twitter'].item()).replace('"', '')
+    pub_twitter = str(data.loc[data['Article URL'] == url, 'Publication Twitter'].item()).replace('"', '')
+    author_fb = str(data.loc[data['Article URL'] == url, 'Author FB'].item()).replace('"', '')
+    pub_fb = str(data.loc[data['Article URL'] == url, 'Publication FB'].item()).replace('"', '')
+    author_ig = str(data.loc[data['Article URL'] == url, 'Author IG'].item()).replace('"', '')
+    pub_ig = str(data.loc[data['Article URL'] == url, 'Publication IG'].item()).replace('"', '')
+    author_linkedin = str(data.loc[data['Article URL'] == url, 'Author LinkedIn'].item()).replace('"', '')
+    pub_linkedin = str(data.loc[data['Article URL'] == url, 'Publication LinkedIn'].item()).replace('"', '')
+    paywall = str(data.loc[data['Article URL'] == url, 'Default Paywall  (Y/N)'].item()).replace('"', '')
+
+    if 'nan' in author_twitter:
+        author_twitter = str(data.loc[data['Article URL'] == url, 'Author Name'].item()).replace('"', '')
+    if 'nan' in pub_twitter:
+        pub_twitter = str(data.loc[data['Article URL'] == url, 'Publication Name'].item()).replace('"', '')
+
+    if 'nan' in author_fb:
+        author_fb = str(data.loc[data['Article URL'] == url, 'Author Name'].item()).replace('"', '')
+    if 'nan' in pub_fb:
+        pub_fb = str(data.loc[data['Article URL'] == url, 'Publication Name'].item()).replace('"', '')
+
+    if 'nan' in author_ig:
+        author_ig = str(data.loc[data['Article URL'] == url, 'Author Name'].item()).replace('"', '')
+    if 'nan' in pub_ig:
+        pub_ig = str(data.loc[data['Article URL'] == url, 'Publication Name'].item()).replace('"', '')
+
+    if 'nan' in author_linkedin:
+        author_linkedin = str(data.loc[data['Article URL'] == url, 'Author Name'].item()).replace('"', '')
+    if 'nan' in pub_linkedin:
+        pub_linkedin = str(data.loc[data['Article URL'] == url, 'Publication Name'].item()).replace('"', '')
+
+    if 'N' in paywall:
+        paywall = ''
+    elif 'Y' in paywall:
+        paywall = '($)' 
+        
+    sentence = sentence.replace('..', '')
+    post = f'"{header}" by {author_twitter} for {pub_twitter}: {sentence}.. {paywall}{link} Twit($)ter'
+    post_item = {
+        'Text': post,
+        'Date': my_date,
+        'Post Link': link
+    }
+    post_item_list.append(post_item)
+    post = f'"{header}" by {author_fb} for {pub_fb}: {sentence}.. {paywall}{link} Face($)book'
+    post_item = {
+        'Text': post,
+        'Date': my_date,
+        'Post Link': link
+    }
+    post_item_list.append(post_item)
+    post = f'"{header}" by {author_ig} for {pub_ig}: {sentence}.. {paywall}{link} I($)G'
+    post_item = {
+        'Text': post,
+        'Date': my_date,
+        'Post Link': link
+    }
+    post_item_list.append(post_item)
+    post = f'"{header}" by {author_linkedin} for {pub_linkedin}: {sentence}.. {paywall}{link} Linked($)in'
+    post_item = {
+        'Text': post,
+        'Date': my_date,
+        'Post Link': link
+    }
+    post_item_list.append(post_item)
+
+    item = {
+        'Date': my_date,
+        'Link': link,
+        'Title': header,
+        'Sentence': sentence,
+        'publication_name' : data.loc[data['Article URL'] == url, 'Publication Name'].item(),
+        'author_name' : data.loc[data['Article URL'] == url, 'Author Name'].item(),
+        'author_twitter' : data.loc[data['Article URL'] == url, 'Author Twitter'].item(),
+        'author_ig' : data.loc[data['Article URL'] == url, 'Author IG'].item(),
+        'author_fb' : data.loc[data['Article URL'] == url, 'Author FB'].item(),
+        'author_linkedin' : data.loc[data['Article URL'] == url, 'Author LinkedIn'].item(),
+        'publication_twitter' : data.loc[data['Article URL'] == url, 'Publication Twitter'].item(),
+        'publication_ig' : data.loc[data['Article URL'] == url, 'Publication IG'].item(),
+        'publication_fb' : data.loc[data['Article URL'] == url, 'Publication FB'].item(),
+        'publication_linkedin' : data.loc[data['Article URL'] == url, 'Publication LinkedIn'].item(),
+        'paywall' : data.loc[data['Article URL'] == url, 'Default Paywall  (Y/N)'].item()
+        
+    }
+    item_list.append(item)
+
+def nytimes_scraper():
+    today = datetime.now()
+    data = pd.read_excel("C:/Users/lenovo/Downloads/bbwaa roster(1).xlsx")
+    urls = data['Article URL'][data['Publication Name'] == 'The New York Times']
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        posts = soup.select('.css-112uytv')
+
+        posts = soup.select('.css-112uytv')
+        for post in posts:
+            link_date = post.select_one('.css-1l4spti a')['href']
+            date = link_date.split('/sports/baseball')
+            date = date[0].replace('/', '')
+            try:
+                date = datetime.strptime(date, "%Y%m%d")
+                my_date = date.strftime("%Y, %m, %d")
+            except:
+                date = datetime.strptime('20230215', "%Y%m%d")
+                my_date = date.strftime("%Y, %m, %d")
+                
+            delta = today - date
+
+            if delta < timedelta(days=3):
+                link = 'https://www.nytimes.com' + link_date
+                header = post.select_one('h2').text
+                sentence = post.select_one('p').text
+
+                add_up(data, url, link, header, sentence, my_date)
+            else:
+               pass
+        
+def forbes_scraper():
+    today = datetime.now()
+    data = pd.read_excel("C:/Users/lenovo/Downloads/bbwaa roster(1).xlsx")
+    urls = data['Article URL'][data['Publication Name'] == 'Forbes']
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        posts = soup.select('article')
+        for post in posts:
+            date = post['data-date']
+            try:
+                date = datetime.fromtimestamp(int(date) / 1000.0)
+                my_date = date.strftime("%Y, %m, %d")
+            except:
+                date = datetime.strptime('20230215', "%Y%m%d")
+                my_date = date.strftime("%Y, %m, %d")
+                
+            delta = today - date
+
+            if delta < timedelta(days=3):
+                link = post.select_one('.stream-item__title')['href']
+                header = post.select_one('.stream-item__title').text
+                sentence = post.select_one('.stream-item__description').text
+
+                add_up(data, url, link, header, sentence, my_date)
+            else:
+                pass
+
+def nj_scraper():
+    today = datetime.now()
+    data = pd.read_excel("C:/Users/lenovo/Downloads/bbwaa roster(1).xlsx")
+    urls = data['Article URL'][data['Publication Name'] == 'NJ.com']
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        posts = soup.select('.river-item')
+
+        for post in posts:
+            date = post.select_one('time')['datetime']
+            try:
+                date = datetime.fromtimestamp(int(date))
+                my_date = date.strftime("%Y, %m, %d")
+            except:
+                date = datetime.strptime('20230215', "%Y%m%d")
+                my_date = date.strftime("%Y, %m, %d")           
+
+            delta = today - date
+            if delta < timedelta(days=3):
+                link = post.select_one('a.river-item__headline-link')['href']
+                header = post.select_one('h2').text
+                sentence = post.select_one('p').text
+
+                add_up(data, url, link, header, sentence, my_date)
+            else:
+                pass
+
+class NewsScraper:
+    
+    @staticmethod
+    def scrapers():
+        nytimes_scraper()
+        forbes_scraper()
+        return item_list, post_item_list
---
+++ app2.py
@@ -0,0 +1,162 @@
+
+from latest_user_agents import get_random_user_agent
+import csv
+import re
+import pandas as pd
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from all_scraper import NewsScraper
+
+def add_paywall(text, symb):
+# define the pattern to select the link
+    pattern = r'(https?://\S+)'
+
+    # find the link in the text
+    link = re.findall(pattern, text)[0]
+
+    # add text to the link
+    new_text = text.replace(link, f'{symb}{link}')
+
+    return new_text
+
+def add_hash_tags(text, symb):
+# define the pattern to select the link
+    pattern = r'(https?://\S+)'
+
+    # find the link in the text
+    link = re.findall(pattern, text)[0]
+
+    # add text to the link
+    new_text = text.replace(link, f'{link}{symb}')
+
+    return new_text
+
+def empty_database():
+    # Empty the CSV file
+    with open('temp_database.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Text'])
+
+def create_database():
+    # Create the CSV file if it doesn't exist and add data if it is empty
+    try:
+        df = pd.read_csv('temp_database.csv')
+        if df.empty:
+            scraper = NewsScraper()
+            info, post = scraper.scrapers()
+            df_info = pd.DataFrame(info)
+            df_post = pd.DataFrame(post)
+            df_post.to_csv('temp_database.csv', index=False)
+    except FileNotFoundError:
+        scraper = NewsScraper()
+        info, post = scraper.scrapers()
+        df_info = pd.DataFrame(info)
+        df_post = pd.DataFrame(post)
+        df_post.to_csv('temp_database.csv', index=False)
+
+def delete_rows(selected_rows):
+    # Delete selected rows from the CSV file
+    df = pd.read_csv('temp_database.csv')
+    selected_df = df.iloc[selected_rows, :]
+    selected_df.to_csv('blacklist_database.csv', index=False)
+    df.drop(selected_rows, inplace=True)
+    df.to_csv('temp_database.csv', index=False)
+
+def add_rows_to_new_database(selected_rows):
+    # Add selected rows to a new CSV file
+    df = pd.read_csv('temp_database.csv')
+    selected_df = df.iloc[selected_rows, :]
+    selected_df.to_csv('commit_database.csv', index=False)
+    selected_df.to_csv('blacklist_database.csv', index=False)
+    df.drop(selected_rows, inplace=True)
+    df.to_csv('temp_database.csv', index=False)
+
+
+def main():
+    # Create the initial database
+    create_database()
+    
+    # Read data from the database
+    df = pd.read_csv('temp_database.csv')
+
+    # Display the rows in a table with checkboxes to select rows for deletion or addition to new database
+    selected_rows = []
+    for index, row in df.iterrows():
+        col1, col2, col3, col4, col5 = st.columns([5,2,2,2,2])
+        checkbox = col2.checkbox("", key=index)
+        if checkbox:
+            selected_rows.append(index)
+        
+        if 'Twit($)ter' in row['Text']:
+            col1.write(row['Text'].replace('Twit($)ter', ''))
+            col2.write('Twitter')
+            col2.write(row['Date'])
+        elif 'Face($)book' in row['Text']:
+            col1.write(row['Text'].replace('Face($)book', ''))
+            col2.write('Facebook')
+            col2.write(row['Date'])
+        elif 'I($)G' in row['Text']:
+            col1.write(row['Text'].replace('I($)G', ''))
+            col2.write('IG')
+            col2.write(row['Date'])
+        elif 'Linked($)in' in row['Text']:
+            col1.write(row['Text'].replace('Linked($)in', ''))
+            col2.write('Linkedin')
+            col2.write(row['Date'])
+        col1.write('---------------------------------------')
+        yankees_button = col3.button(f"Yankees {index}")
+        mets_button = col4.button(f"Mets {index}")
+        paywall_button = col5.button(f'Paywall {index}')
+
+
+        # Add exclamation mark to rows
+        with st.container():
+            #add $ sign to the posts
+            if '#$' not in row['Text']:
+                if paywall_button:
+                    df.at[index, 'Text'] = add_paywall(row['Text'], '($)')
+                    df.to_csv('temp_database.csv', index=False)
+            else:
+                if paywall_button:
+                    df.at[index, 'Text'] = row['Text'].replace('($)', '')
+                    df.to_csv('temp_database.csv', index=False)
+
+            #add #Yankees hashtags to the post
+            if '#Yankees' not in row['Text']:
+                if yankees_button:
+                    df.at[index, 'Text'] = add_hash_tags(row['Text'], '#Yankees')
+                    df.to_csv('temp_database.csv', index=False)
+            else:
+                if yankees_button:
+                    df.at[index, 'Text'] = row['Text'].replace('#Yankees', '')
+                    df.to_csv('temp_database.csv', index=False)
+
+            # Add #Mets hashtags to post
+            if '#Mets' not in row['Text']:
+                if mets_button:
+                    df.at[index, 'Text'] = add_hash_tags(row['Text'], '#Mets')
+                    df.to_csv('temp_database.csv', index=False)
+            else:
+                if mets_button:
+                    df.at[index, 'Text'] = row['Text'].replace('#Mets', '')
+                    df.to_csv('temp_database.csv', index=False)
+
+    # Add a button to delete selected rows
+    if st.button("Delete selected rows"):
+        delete_rows(selected_rows)
+        st.success("Selected rows deleted successfully")
+    
+    # Add a button to add selected rows to new database
+    if st.button("Add selected rows to new database"):
+        add_rows_to_new_database(selected_rows)
+        st.success("Selected rows added to new database successfully")
+
+    # Add an 'Empty database' button to the app
+    if st.button('Empty database'):
+        empty_database()
+        st.write('Database has been emptied!')
+
+if __name__ == '__main__':
+    main()
