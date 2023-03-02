b6ae6f81c22c1b0ef1641386966aa254d3394a38
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-02 17:18:55 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -15,11 +15,9 @@ if 'engine' not in st.session_state:
     engine = create_engine(f"mysql+pymysql://{uname}:{pwd}@{hostname}/{dbname}")
     st.session_state['engine'] = engine
 
-@st.cache_data
 def convert_df(df):
    return df.to_csv(index=False).encode('utf-8')
 
-@st.cache_data
 def delete_blacklisted(df_a):
     engine = st.session_state['engine']
     conn = engine.connect()
@@ -39,7 +37,6 @@ def downlaod_commited():
     csv = convert_df(df)
     return csv
 
-@st.cache_data
 def add_paywall(text, symb):
 # define the pattern to select the link
     pattern = r'(https?://\S+)'
@@ -52,7 +49,6 @@ def add_paywall(text, symb):
 
     return new_text
 
-@st.cache_data
 def add_hash_tags(text, symb):
 # define the pattern to select the link
     pattern = r'(https?://\S+)'
@@ -65,7 +61,6 @@ def add_hash_tags(text, symb):
 
     return new_text
 
-@st.cache_data
 def empty_database():
     df1 = pd.DataFrame()
     df2 = pd.DataFrame()
@@ -93,7 +88,6 @@ def create_database():
     clean_post = delete_blacklisted(df_post)
     clean_post.to_csv('temp_database.csv', index=False, encoding='utf-8') #to correct later
 
-@st.cache_data
 def delete_rows(selected_rows):
     engine = st.session_state['engine']
     selected_df = st.session_state['data2'].loc[selected_rows, :]
@@ -106,7 +100,6 @@ def delete_rows(selected_rows):
             st.session_state['data2'] = st.session_state['data2'].drop(index)
     st.experimental_rerun()
 
-@st.cache_data
 def add_rows_to_new_database(selected_rows):
     engine = st.session_state['engine']
     selected_df = st.session_state['data2'].loc[selected_rows, :]
