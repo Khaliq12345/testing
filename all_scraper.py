9ff376b4ca6605cd807cd692ea7edb2de20189f6
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-09 01:34:46 +0000

    Update all_scraper.py

--- all_scraper.py
+++ all_scraper.py
@@ -1473,7 +1473,7 @@ class NewsScraper:
         # cbs_sports_scraper()
         # ringer_scraper()
         # sportsbusinessjournal_scraper()
-        #yahoo_scraper()
+        yahoo_scraper()
         # nypost_scraper()
         # foxsports_scraper()
         # insider_scraper()
