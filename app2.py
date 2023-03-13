0bab4989b6ed0019c3cfdd2efdfec65c20a8fe51
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-13 23:05:22 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -192,7 +192,6 @@ for index, row in st.session_state['data1'][:40].iterrows():
         col2.write("Twitter")
         date_obj = datetime.strptime(row["Date"], "%Y, %m, %d").strftime("%B %d, %Y")
         col2.write(date_obj)
-        col2.image(row['Image'])
     elif "Face($)book" in row["Text"]:
         edited_text = col1.text_area(f'post_{index}',row["Text"].replace("Face($)book", "").strip(), height=150)
         st.session_state['data2'].at[index, 'Text'] = edited_text
