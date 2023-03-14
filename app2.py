213664ef0db0331050f2fecbd8631d3a94a3367d
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-14 14:18:46 +0000

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,12 +11,6 @@ import cv2
 from html2image import Html2Image
 hti = Html2Image()
 
-def get_image(l):
-    hti.screenshot(url=l, save_as='image.png')
-    image_data = cv2.imread('image.png')
-
-    return image_data
-
 if 'engine' not in st.session_state:
     hostname=st.secrets['hostname']
     dbname=st.secrets['dbname']
@@ -202,9 +196,9 @@ for index, row in st.session_state['data1'][:40].iterrows():
         col2.write("Twitter")
         date_obj = datetime.strptime(row["Date"], "%Y, %m, %d").strftime("%B %d, %Y")
         col2.write(date_obj)
-        if f'image_{index}' not in st.session_state:
-            st.session_state[f'image_{index}'] = get_image(row['Post Link'])
-        col2.image(st.session_state[f'image_{index}'])
+        #if f'image_{index}' not in st.session_state:
+            #st.session_state[f'image_{index}'] = get_image(row['Post Link'])
+        #col2.image(st.session_state[f'image_{index}'])
         
     elif "Face($)book" in row["Text"]:
         edited_text = col1.text_area(f'post_{index}',row["Text"].replace("Face($)book", "").strip(), height=150)
