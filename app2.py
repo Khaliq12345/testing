0e5da462a8fb90a319651482ed2e6177f91bddfc
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-14 19:53:03 +0000

    h

--- app2.py
+++ app2.py
@@ -207,7 +207,6 @@ for index, row in st.session_state['data1'][:40].iterrows():
             hti.screenshot(url=row['Post Link'], save_as='image.png')
             image_data = cv2.imread('image.png')
             st.session_state[f'image_{index}'] = image_data
-        st.session_state[f'image_{index}']
         col2.image(image_data)
         #st.write('Yes')
         
