31474840f0e3a85e505fe69709349867ee13af01
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-15 01:15:55 +0000

    k

--- app2.py
+++ app2.py
@@ -199,9 +199,9 @@ for index, row in st.session_state['data1'][:40].iterrows():
         col2.write("Twitter")
         date_obj = datetime.strptime(row["Date"], "%Y, %m, %d").strftime("%B %d, %Y")
         col2.write(date_obj)
-        if f'img_{index}' not in st.session_state:
-            st.session_state[f'img_{index}'] = get_image(row['Post Link'], f'image_{index}.png')
-        col2.image(f'image_{index}.png')
+        #if f'img_{index}' not in st.session_state:
+            #st.session_state[f'img_{index}'] = get_image(row['Post Link'], f'image_{index}.png')
+        #col2.image(f'image_{index}.png')
 
     elif "Face($)book" in row["Text"]:
         edited_text = col1.text_area(f'post_{index}',row["Text"].replace("Face($)book", "").strip(), height=150)
