62984ff35ac8b646602c678e2907db8f328f3e31
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-03-20 23:10:50 +0000

    ddd

--- app2.py
+++ app2.py
@@ -15,7 +15,7 @@ def get_image(link, image):
         browser = p.chromium.launch()
         page = browser.new_page()
         page.goto(link)
-        page.screenshot(path=f"{image}.png")
+        page.screenshot(path=f"{image}.png", full_page=True)
         browser.close()
 
 if 'engine' not in st.session_state:
